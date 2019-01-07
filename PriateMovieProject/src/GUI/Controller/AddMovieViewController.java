8767cb11edeb789c868dc256a97e6968ab62d888
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2019-01-07 13:11:47 +0100

    Category choose btn works!

--- PriateMovieProject/src/GUI/Controller/AddMovieViewController.java
+++ PriateMovieProject/src/GUI/Controller/AddMovieViewController.java
@@ -5,12 +5,15 @@
  */
 package GUI.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -59,10 +62,15 @@ public class AddMovieViewController implements Initializable
     }
 
     @FXML
-    private void handleCategoryChooseBtn(ActionEvent event)
+    private void handleCategoryChooseBtn(ActionEvent event) throws IOException
     {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/CategoryView.fxml"));
+        Parent root = (Parent) loader.load();
         
+        Stage stage = new Stage();
+        stage.setTitle("Movie collection");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
     
 }
