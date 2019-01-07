a828a89dd2a26b111e0c9a5b30d2261de41f10ae
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-01-07 11:31:37 +0100

    Implementeret business entiteter

--- PriateMovieProject/src/BE/Category.java
+++ PriateMovieProject/src/BE/Category.java
@@ -11,5 +11,28 @@ package BE;
  */
 public class Category
 {
+    private int id;
+    private String category;
+    
+    public Category(int id, String category) {
+        this.id = id;
+        this.category = category;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getCategory()
+    {
+        return category;
+    }
+
+    public void setCategory(String category)
+    {
+        this.category = category;
+    }
+    
     
 }
--- PriateMovieProject/src/BE/Movie.java
+++ PriateMovieProject/src/BE/Movie.java
@@ -11,5 +11,62 @@ package BE;
  */
 public class Movie
 {
+    private int id;
+    private String name;
+    private double rating;
+    private String filepath;
+    private int lastview;
     
+    public Movie(int id, String name, double rating, String filepath, int lastview) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.filepath = filepath;
+        this.lastview = lastview;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public double getRating()
+    {
+        return rating;
+    }
+
+    public String getFilepath()
+    {
+        return filepath;
+    }
+
+    public int getLastview()
+    {
+        return lastview;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setRating(double rating)
+    {
+        this.rating = rating;
+    }
+
+    public void setFilepath(String filepath)
+    {
+        this.filepath = filepath;
+    }
+
+    public void setLastview(int lastview)
+    {
+        this.lastview = lastview;
+    }
 }
