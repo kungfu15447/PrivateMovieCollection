cd9372ac1838a486f8d1f71c8e36d09679c1c395
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-01-08 11:15:01 +0100

    Begyndt at arbejde på CatMovieDAO klassen

--- PriateMovieProject/src/DAL/CatMovieDAO.java
+++ PriateMovieProject/src/DAL/CatMovieDAO.java
@@ -5,11 +5,30 @@
  */
 package DAL;
 
+import BE.Category;
+import BE.Movie;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Frederik Jensen
  */
 public class CatMovieDAO
 {
+    private final ConnectionDAO cb;
     
+    public CatMovieDAO() {
+        cb = new ConnectionDAO();
+    }
+    
+    public List<Movie> getMoviesFromCats(List<Category> catlist) throws SQLException {
+        List<Movie> catmovies = new ArrayList<>();
+        try (Connection con = cb.getConnection()) {
+            String sql = "SELECT * FROM ";
+        }
+        return catmovies;
+    }
 }
