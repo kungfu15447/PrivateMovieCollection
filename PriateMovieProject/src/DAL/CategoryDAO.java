2abaf64ec67fae1e6c946aec320292924679194f
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-01-08 10:26:02 +0100

    Tilføjet en getAllCategories metode

--- PriateMovieProject/src/DAL/CategoryDAO.java
+++ PriateMovieProject/src/DAL/CategoryDAO.java
@@ -5,11 +5,39 @@
  */
 package DAL;
 
+import BE.Category;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Frederik Jensen
  */
 public class CategoryDAO
 {
+    private final ConnectionDAO cb;
     
+    public CategoryDAO() {
+        cb = new ConnectionDAO();
+    }
+    
+    public List<Category> getAllCategories() throws SQLException {
+        List<Category> catList = new ArrayList<>();
+        
+        try (Connection con = cb.getConnection()) {
+            String sql = "SELECT * FROM Category;";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+            }
+            
+        }
+        return catList;
+    }
 }
