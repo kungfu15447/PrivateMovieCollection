772aad53cef9176bdca6538f69bd59db23f61d9f
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-01-15 18:05:40 +0100

    Lavet en metode der downloader zip filer fra imdb hjemmeside

--- PriateMovieProject/src/DAL/ImdbDAO.java
+++ PriateMovieProject/src/DAL/ImdbDAO.java
@@ -12,11 +12,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.GZIPInputStream;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -28,8 +31,8 @@ public class ImdbDAO
     public void unzipRating()
     {
 
-        String inputGZFile = "data/update/title.ratings.tsv.gz";
-        String outputFile = "data/update/data.tsv";
+        String inputGZFile = "data/title.ratings.tsv.gz";
+        String outputFile = "data/data.tsv";
         byte[] buffer = new byte[1024];
         try (GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(inputGZFile)); FileOutputStream out = new FileOutputStream(outputFile))
         {
@@ -47,8 +50,8 @@ public class ImdbDAO
 
     public void unzipTitle()
     {
-        String inputGZFile = "data/update/title.basics.tsv.gz";
-        String outputFile = "data/update/data.tsv";
+        String inputGZFile = "data/title.basics.tsv.gz";
+        String outputFile = "data/data.tsv";
         byte[] buffer = new byte[1024];
         try (GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(inputGZFile)); FileOutputStream out = new FileOutputStream(outputFile))
         {
@@ -102,5 +105,46 @@ public class ImdbDAO
         return "No rating found";
     }
     
+    public void downloadIMDBDatabase() {
+        try {
+            String imdbRatingsURL = "https://datasets.imdbws.com/title.ratings.tsv.gz";
+            String localFile = "data/title.ratings.tsv.gz";
+            
+            URL url = new URL(imdbRatingsURL);
+            File file = new File(localFile);
+            
+            FileUtils.copyURLToFile(url, file, 10000, 10000);
+            
+            String imdbBasicsURL = "https://datasets.imdbws.com/title.basics.tsv.gz";
+            String localFile2 = "data/title.basics.tsv.gz";
+            
+            URL url2 = new URL(imdbBasicsURL);
+            File file2 = new File(localFile2);
+            
+            FileUtils.copyURLToFile(url, file, 10000, 10000);
+            
+            File deleteOldRating = new File("data/rating.tsv");
+            File deleteOldTitle = new File("data/title.tsv");
+            deleteOldRating.delete();
+            deleteOldTitle.delete();
+            
+            unzipRating();
+            
+            File data1 = new File("data/data.tsv");
+            File rating = new File("data/rating.tsv");
+            data1.renameTo(rating);
+            
+            unzipTitle();
+            
+            File data2 = new File("data/data.tsv");
+            File title = new File("data/title.tsv");
+            
+            File deleteBasicZip = new File("data/title.basics.tsv.gz");
+            File deleteRatingZip = new File("data/title.ratings.tsv.gz");
+            
+        }  catch (IOException ex) {
+            Logger.getLogger(ImdbDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
 }
