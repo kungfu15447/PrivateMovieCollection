87820d43d6d2c6439f51f7f174ac399861f62300
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-02-19 11:31:03 +0100

    Code smell refractor

--- PriateMovieProject/src/BLL/MovieFilter.java
+++ PriateMovieProject/src/BLL/MovieFilter.java
@@ -24,7 +24,6 @@ public class MovieFilter
     MovieDAO modao;
     CatMovieDAO catdao;
 
-    
     /**
      * The constructer of the MovieFilter class
      */
@@ -35,15 +34,15 @@ public class MovieFilter
     }
 
     /**
-     * Returns a list of movies whose titles contains a searched word
-     * If the searched word is empty the method either returns 
-     * a list containing all movies or a list containing movies based on 
-     * specific selected categories.
+     * Returns a list of movies whose titles contains a searched word If the
+     * searched word is empty the method either returns a list containing all
+     * movies or a list containing movies based on specific selected categories.
+     *
      * @param searchBase the list getting searched through
      * @param categoryList the list of selected categories
      * @param query the searched word
      * @return SearchList
-     * @throws MTBllException 
+     * @throws MTBllException
      */
     public List<Movie> searchFilter(List<Movie> searchBase, List<Category> categoryList, String query) throws MTBllException
     {
@@ -81,32 +80,33 @@ public class MovieFilter
 
     /**
      * Returns a filtered list of movies based on a list of selected categories
-     * This method gets a list of all movies from the CategoryMovie table
-     * This list will have a bunch of duiplicates of the same movie objects. 
-     * The only difference in these object is their list contains one integer.
-     * That integer represent the id of a category. So if a movie object has 
-     * three categories then that movie will occur three times in this list. 
-     * After this the method then gets a list of selected categories and takes 
-     * every category's id and inserts them into a new list. It then checks if 
-     * that list only contains one id or multiple. If it has multiple category 
-     * ids the method then goes into a loop where it checks every movie except 
-     * the first one and compares them to a temporary movie object 
-     * which is the first movie in that list. If the movie title is the same as
-     * the temporary movie object then the list containing one category id 
-     * will be added to the movie object. If the list finds a movie with a unique name
-     * then that movie will be the new temporary movie object. 
-     * So we will end up with a list where there is still duplicates of movies 
-     * but the first occurence of a new movie will have all its categories id in its list. 
-     * The method then goes through every movie again and checks if its list of categories id 
-     * is identical to the list of selected categories which was made at the start of the method. 
-     * If they are identical then they get added to the filtered list. 
-     * After the loop is done the list is then returned.
-     * Also if the list of categories is not greater than one then it doesnt
-     * go through this whole process. Then it just checks if the movies contains 
-     * that one category. And if they do then they are added to the filtered list
+     * This method gets a list of all movies from the CategoryMovie table This
+     * list will have a bunch of duiplicates of the same movie objects. The only
+     * difference in these object is their list contains one integer. That
+     * integer represent the id of a category. So if a movie object has three
+     * categories then that movie will occur three times in this list. After
+     * this the method then gets a list of selected categories and takes every
+     * category's id and inserts them into a new list. It then checks if that
+     * list only contains one id or multiple. If it has multiple category ids
+     * the method then goes into a loop where it checks every movie except the
+     * first one and compares them to a temporary movie object which is the
+     * first movie in that list. If the movie title is the same as the temporary
+     * movie object then the list containing one category id will be added to
+     * the movie object. If the list finds a movie with a unique name then that
+     * movie will be the new temporary movie object. So we will end up with a
+     * list where there is still duplicates of movies but the first occurence of
+     * a new movie will have all its categories id in its list. The method then
+     * goes through every movie again and checks if its list of categories id is
+     * identical to the list of selected categories which was made at the start
+     * of the method. If they are identical then they get added to the filtered
+     * list. After the loop is done the list is then returned. Also if the list
+     * of categories is not greater than one then it doesnt go through this
+     * whole process. Then it just checks if the movies contains that one
+     * category. And if they do then they are added to the filtered list
+     *
      * @param catlist the list of selected categories
      * @return the list of filtered movies based on the selected categories
-     * @throws MTBllException 
+     * @throws MTBllException
      */
     public List<Movie> categoriFilter(List<Category> catlist) throws MTBllException
     {
@@ -114,41 +114,19 @@ public class MovieFilter
         {
             List<Movie> movieCatList = catdao.getMoviesFromCats();
             List<Movie> filteredList = new ArrayList<>();
-            List<Integer> catIdList = new ArrayList<>();
+            List<Integer> catIdList = getCategoryIdList(catlist);
             Movie tempMovie = movieCatList.get(0);
 
-            for (Category cat : catlist)
+            if (catlist.size() > 1)
             {
-                catIdList.add(cat.getId());
+                addCategoriesToMovie(movieCatList, tempMovie);
             }
-            if (catIdList.size() == 1)
-            {
-                for (Movie movie : movieCatList)
-                {
-                    if (movie.getList().equals(catIdList))
-                    {
-                        filteredList.add(movie);
-                    }
-                }
-            } else
-            {
-                for (int i = 1; i < movieCatList.size(); i++)
-                {
-                    if (tempMovie.getName().equals(movieCatList.get(i).getName()))
-                    {
-                        tempMovie.getList().add(movieCatList.get(i).getList().get(0));
-                    } else
-                    {
-                        tempMovie = movieCatList.get(i);
-                    }
-                }
 
-                for (Movie movie : movieCatList)
+            for (Movie movie : movieCatList)
+            {
+                if (movie.getList().equals(catIdList))
                 {
-                    if (movie.getList().equals(catIdList))
-                    {
-                        filteredList.add(movie);
-                    }
+                    filteredList.add(movie);
                 }
             }
 
@@ -157,5 +135,29 @@ public class MovieFilter
         {
             throw new MTBllException();
         }
+    }
+
+    private void addCategoriesToMovie(List<Movie> movieList, Movie tempMovie)
+    {
+        for (int i = 1; i < movieList.size(); i++)
+        {
+            if (tempMovie.getName().equals(movieList.get(i).getName()))
+            {
+                tempMovie.getList().add(movieList.get(i).getList().get(0));
+            } else
+            {
+                tempMovie = movieList.get(i);
+            }
+        }
+    }
+
+    private List<Integer> getCategoryIdList(List<Category> catlist)
+    {
+        List<Integer> catIdList = new ArrayList<>();
+        for (Category cat : catlist)
+        {
+            catIdList.add(cat.getId());
+        }
+        return catIdList;
     }
 }
